9fb142573b1db4537f755b94916fc4f2ddc85b81
Thanatus <peetthanatus@gmail.com>
2024-10-11 19:12:43 +0700

    add theme css

--- App/src/main/java/ku/cs/controller/loginController.java
+++ App/src/main/java/ku/cs/controller/loginController.java
@@ -1,4 +1,21 @@
 package ku.cs.controller;
 
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+
 public class loginController {
+
+    @FXML
+    public ImageView bgLogin;
+
+
+    public void initialize() {
+        URL url = getClass().getResource("/ku/cs/picture/backgroundlogin.png");
+        if (url != null) bgLogin.setImage(new Image(url.toExternalForm()));
+
+    }
 }
