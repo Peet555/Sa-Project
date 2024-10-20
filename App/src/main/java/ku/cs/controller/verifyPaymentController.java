614905cda557d9490c410bf862675da4360793f8
Palm Thamakorn <thamakorn.t@ku.th>
2024-10-20 18:07:47 +0700

    add paymentverify page and fix layout stock page

---
+++ App/src/main/java/ku/cs/controller/verifyPaymentController.java
@@ -0,0 +1,9 @@
+package ku.cs.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+
+public class verifyPaymentController {
+    @FXML
+    public TableView statusPayment ;
+}
