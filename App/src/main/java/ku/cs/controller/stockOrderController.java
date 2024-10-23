a2ae597599be7be11788300718debff2427788d5
Palm Thamakorn <thamakorn.t@ku.th>
2024-10-23 18:46:22 +0700

    add stock order page

---
+++ App/src/main/java/ku/cs/controller/stockOrderController.java
@@ -0,0 +1,20 @@
+package ku.cs.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class stockOrderController {
+
+    @FXML
+    public ImageView logo ;
+
+    @FXML
+    public TextField idStockOrder ;
+
+    @FXML
+    public TableView quantityStockOrder ;
+
+    
+}
