d7061007b35374bc795bb6fb5fc69d11f40d16bd
Dewchayanon <chayanonkl118@gmail.com>
2024-10-24 19:00:41 +0700

    Create_editProfileWindow

--- App/src/main/java/ku/cs/controller/profileController.java
+++ App/src/main/java/ku/cs/controller/profileController.java
@@ -1,8 +1,13 @@
 package ku.cs.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import ku.cs.services.FXRouter;
 import java.io.IOException;
 
@@ -36,9 +41,21 @@ public class profileController {
     @FXML
     public Button profileButton ; // profile
 
+    @FXML
+    public Button editProfileButton ; // edit profile
+
 
     @FXML
     public void initialize() throws IOException {
+            // กำหนดการทำงานของปุ่มชำระเงิน
+        editProfileButton.setOnAction(event -> {
+                try {
+                    openEditProfileWindow();
+                } catch (IOException e) {
+                    System.err.println("Error opening payment window: " + e.getMessage());
+                }
+            });
+
 
         homeButton.setOnAction(event -> {
             try {
@@ -71,6 +88,17 @@ public class profileController {
             }
         });
 
+    }
+    // Method สำหรับเปิดหน้าต่าง paymentOrderWindow
+    private void openEditProfileWindow() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ku/cs/view/editProfileWindow.fxml"));
+        Parent root = loader.load();
+
+        // สร้าง Stage สำหรับหน้าต่างใหม่
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.initModality(Modality.APPLICATION_MODAL);  // หน้าต่างใหม่จะเป็นแบบ modal (โฟกัสเฉพาะหน้าต่างนี้)
+        stage.showAndWait();  // แสดงหน้าต่าง
     }
 }
 
