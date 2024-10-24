c01d458b8f5d8788510a1086516d0f756b4e3121
Palm Thamakorn <thamakorn.t@ku.th>
2024-10-24 20:41:31 +0700

    fix check proof table and page

--- App/src/main/java/ku/cs/controller/checkProofPaymentController.java
+++ App/src/main/java/ku/cs/controller/checkProofPaymentController.java
@@ -1,10 +1,14 @@
 package ku.cs.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -14,10 +18,36 @@ import java.io.IOException;
 
 public class checkProofPaymentController {
     @FXML
-    public TableView paymentList ;
+    private TableView<ProductSale> paymentList ;
+
+    @FXML
+    private TableColumn<ProductSale,String> id ;
+    @FXML
+    private TableColumn<ProductSale,String> name ;
+    @FXML
+    private TableColumn<ProductSale,Integer> quantity ;
+    @FXML
+    private TableColumn<ProductSale,Integer> productPrice ;
+    @FXML
+    private TableColumn<ProductSale,Integer> price ;
 
     @FXML
     public ImageView proofPayment ;
+
+    @FXML
+    public void initialize(){
+        id.setCellValueFactory(new PropertyValueFactory<>("productID"));
+        name.setCellValueFactory(new PropertyValueFactory<>("productName"));
+        quantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        productPrice.setCellValueFactory(new PropertyValueFactory<>("productPrice"));
+        price.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        ObservableList<ProductSale> productSales = FXCollections.observableArrayList(
+                new ProductSale("001", "TV", 2, 1000,2000)
+        );
+
+        paymentList.setItems(productSales);
+    }
 
     @FXML
     public void goVerifyPayment(){
@@ -57,6 +87,60 @@ public class checkProofPaymentController {
         stage.setTitle("Confirmation");
         stage.initModality(Modality.APPLICATION_MODAL);  // หน้าต่างใหม่จะเป็นแบบ modal (โฟกัสเฉพาะหน้าต่างนี้)
         stage.showAndWait();  // แสดงหน้าต่าง
+    }
+
+    @FXML
+    public void backClick(){
+        try {
+            FXRouter.goTo("verifyPayment");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    public void cancleClick() {
+        try {
+            FXRouter.goTo("verifyPayment");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static class ProductSale {
+        private String productID ;
+        private String productName ;
+        private Integer quantity ;
+        private Integer productPrice ;
+        private Integer price ;
+
+        public ProductSale(String productID, String productName, Integer quantity, Integer productPrice, Integer price) {
+            this.productID = productID;
+            this.productName = productName;
+            this.quantity = quantity;
+            this.productPrice = productPrice;
+            this.price = price;
+        }
+
+        public String getProductID() {
+            return productID;
+        }
+
+        public String getProductName() {
+            return productName;
+        }
+
+        public Integer getQuantity() {
+            return quantity;
+        }
+
+        public Integer getProductPrice() {
+            return productPrice;
+        }
+
+        public Integer getPrice() {
+            return price;
+        }
     }
 
 }
