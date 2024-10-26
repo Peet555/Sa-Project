9fd8c76ddd700744205e1fa37bf2da49aa1505fb
Dewchayanon <chayanonkl118@gmail.com>
2024-10-26 12:58:39 +0700

    fix_error_customerProfile

--- App/src/main/java/ku/cs/controller/editEmployeeSellerProfileWindowController.java
+++ App/src/main/java/ku/cs/controller/editEmployeeSellerProfileWindowController.java
@@ -2,11 +2,18 @@ package ku.cs.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class editEmployeeSellerProfileWindowController {
     @FXML
     private Button confirmButton, cancelButton;
+
+    @FXML
+    private TextField nameField,phoneNumberField;
+    @FXML
+    private TextArea addressField;
 
     @FXML
     public void initialize() {
--- App/src/main/java/ku/cs/controller/editProfileWindowController.java
+++ App/src/main/java/ku/cs/controller/editProfileWindowController.java
@@ -3,6 +3,7 @@ package ku.cs.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -13,7 +14,9 @@ public class editProfileWindowController {
     private Button confirmButton, cancelButton;
 
     @FXML
-    private TextField nameField,phoneNumberField,addressField;
+    private TextField nameField,phoneNumberField;
+    @FXML
+    private TextArea addressField;
 
     @FXML
     public void initialize() {
